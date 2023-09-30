302e25fedeb9036d9b299027c8fd06caf67a74dd
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-30 14:53:41 +0100

    ds

--- server.js
+++ server.js
@@ -4,5 +4,11 @@ const app = express()
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
+app.get("/login", (req, res) => {
+    res.render("login.ejs")
+})
+app.get("/register", (req, res) => {
+    res.render("register.ejs")
+})
 
 app.listen(3000)
